8bf6c7e272831d6ad212418885f61e8ad7419272
mlm-games <125530737+mlm-games@users.noreply.github.com>
2026-01-21 20:39:56 +0530

    Win screen focus

--- scenes/UI/win_screen.gd
+++ scenes/UI/win_screen.gd
@@ -2,6 +2,7 @@ class_name WinScreen extends Control
 
 func _ready():
 	STransitions.transition("circleOut")
+	%ContinueButton.grab_focus()
 
 func _on_menu_button_pressed() -> void:
 	STransitions.change_scene_with_transition(Menu.WorldScene)
